adf8f68a0a3a6694dd813eaa33b956c67760eed9
Insso <insso.fooly@gmail.com>
2023-06-16 01:53:09 +0300

    ajout de la fonction pour réinitialiser le jeu

--- Tic-Tac-Toe_project_ABBOUAN-Anas/main.py
+++ Tic-Tac-Toe_project_ABBOUAN-Anas/main.py
@@ -14,6 +14,21 @@ lePlayer = "X"
 resetButton = tkinter.Button(window, text="Réinitialiser")
 resetButton.grid(row=3, column=0, columnspan=3)
 
+# Fonction pour réinitialiser le jeu
+"""La fonction ci-dessous utilise une var global 
+   c'est plus simple pour moi d'avoir une main sur 
+   qui a la main justement"""
+def resetTheGame():
+    global lePlayer
+
+    # relance le tab
+    for i in range(3):
+        for j in range(3):
+            leTab[i][j] = ""
+            lesButtons[i][j].config(text="")
+
+    lePlayer = "X" 
+
 # Créer les boutons pour chaque case
 lesButtons = []
 for i in range(3):
@@ -25,20 +40,6 @@ for i in range(3):
         row.append(button)
     lesButtons.append(row)
 
+
 # Lancer du jeu
 window.mainloop()
-
-#window.title('Anas s GUI V3')
-# label=tkinter.Label(window,text="Ma fst GUI window perso")
-# label= tkinter.Label(window,text="Je use tkinter library",background="blue").pack()
-
-# #clicable button
-# bt=tkinter.Button(text="Cliiiiick euh").pack()
-# window.geometry("300x200+10+10")
-
-# #user input
-# txt=tkinter.Entry(width=20).pack()
-
-# #big text box
-# textBox=tkinter.Text()
-# textBox.pack()
