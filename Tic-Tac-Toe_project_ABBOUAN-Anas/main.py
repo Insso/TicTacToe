e782f8f42438c405beeb8170392102233ded4455
Insso <insso.fooly@gmail.com>
2023-06-16 02:16:01 +0300

    ajout de 2 fct; la première pour vérifier si un joueur a gg et la deuxième qui créer; !!! il reste juste la fct clique à définir

--- Tic-Tac-Toe_project_ABBOUAN-Anas/main.py
+++ Tic-Tac-Toe_project_ABBOUAN-Anas/main.py
@@ -29,17 +29,30 @@ def resetTheGame():
 
     lePlayer = "X" 
 
+# check-up pour savoir si un joueur a gg
+"""À chaque fois qu'une case est cliqué
+   on vérifie si le joueur actuel a gagné"""
+def checkWin(player):
+    for i in range(3):
+        if leTab[i][0] == leTab[i][1] == leTab[i][2] == player or \
+           leTab[0][i] == leTab[1][i] == leTab[2][i] == player:
+            return True
+    if leTab[0][0] == leTab[1][1] == leTab[2][2] == player or \
+       leTab[0][2] == leTab[1][1] == leTab[2][0] == player:
+        return True
+    return False
+
+
 # Créer les boutons pour chaque case
 lesButtons = []
 for i in range(3):
     row = []
     for j in range(3):
         button = tkinter.Button(window, text="", width=10, height=5,
-                               command=lambda row=i, col=j: cell_click(row, col))
+                               command=lambda row=i, col=j: clique(row, col))
         button.grid(row=i, column=j)
         row.append(button)
     lesButtons.append(row)
-
 
 # Lancer du jeu
 window.mainloop()
